eae1b2f54897172f5f50a971070e6fd3592e1e20
Leo Garvanovic <Leo.Garvanovic@training.lug-ag.de>
2025-12-02 09:23:40 +0100

    Neue Ordner und Klassen erstellt

---
+++ projekt_verwaltungssystem_leo_garvanovic/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt_verwaltungssystem_leo_garvanovic.Models
+{
+    internal class Person
+    {
+    }
+}
